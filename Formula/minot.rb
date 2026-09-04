33b5a7e92ab64900415626739e87fe84d5ed1e78
stelzo <stelzo@steado.de>
2026-09-04 19:32:20 +0000

    Update minot to v0.10.2

--- Formula/minot.rb
+++ Formula/minot.rb
@@ -1,15 +1,15 @@
 # Maintainer: Christopher Sieh (stelzo) <stelzo@steado.de>
-# Template file — CI replaces 0.10.1 and 70465603b0533ef99ff634b3db7545807c0278fb2acce927b8da4823051f9a7f before publishing to the tap.
+# Template file — CI replaces 0.10.2 and f0dac16a04d6a10158279649da6cc436db3ce040d9fb5a95c0be820d28a5d518 before publishing to the tap.
 class Minot < Formula
   desc "A versatile toolset for debugging and verifying stateful robot perception software"
   homepage "https://codeberg.org/stelzo/minot"
-  version "0.10.1"
+  version "0.10.2"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     on_arm do
       url "https://codeberg.org/stelzo/minot/releases/download/v#{version}/minot-aarch64-apple-darwin.tar.gz"
-      sha256 "70465603b0533ef99ff634b3db7545807c0278fb2acce927b8da4823051f9a7f"
+      sha256 "f0dac16a04d6a10158279649da6cc436db3ce040d9fb5a95c0be820d28a5d518"
     end
   end
 
